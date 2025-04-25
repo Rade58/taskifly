ba2bda1a9984740558f84c7756c3d6ea508ad742
rade <bajic.rade2@gmail.com>
2025-04-25 16:32:47 +0200

    eslint prettier

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Здраво Свете</Text>
+      <View
+        style={{
+          backgroundColor: 'pink',
+          paddingHorizontal: 8,
+          // width: 100,
+          // height: 100,
+        }}
+      >
+        <Text>Здраво Свете. Šta radiš danas?</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
