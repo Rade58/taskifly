f031a323b16fe78bb112bd0175a553986e8ac864
rade <bajic.rade2@gmail.com>
2025-04-28 20:32:52 +0200

    stack navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import {
   /* PixelRatio, */ StyleSheet,
-  Text,
+  // Text,
   View,
-  TouchableOpacity,
-  Alert,
+  // TouchableOpacity,
+  // Alert,
 } from 'react-native';
+
+import { Link } from 'expo-router';
 
 // PixelRatio.get() will show you how many pixel is per unit of measurement
 // in your device
@@ -16,6 +18,16 @@ import { ShoppingListItem } from '../components/ShoppingListItem';
 export default function App() {
   return (
     <View style={styles.container}>
+      <Link
+        href="/counter"
+        style={{
+          textAlign: 'center',
+          marginBottom: 18,
+          fontSize: 24,
+        }}
+      >
+        Go to /counter
+      </Link>
       <ShoppingListItem isCompleted={false} name="Coffee" />
       <ShoppingListItem isCompleted name="Tea" />
       <ShoppingListItem isCompleted={true} name="Milk" />
